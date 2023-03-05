a3bb05078b4b816d32a08ddab87f57df462aea55
Robert C. Haye <roberthaye1994@gmail.com>
2023-03-05 14:50:12 +0100

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 "use strict";
-const yellow = "yellow";
+let map;
+let mapEvent;
+const latitude = 45.7988904;
+const longitude = 15.8869043;
+map = L.map("map").setView([latitude, longitude], 13);
+L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+L.marker([latitude, longitude])
+    .addTo(map)
+    .bindPopup(L.popup({
+    maxWidth: 150,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+}))
+    .setPopupContent("Savska opatovina 96, 10070, Zagreb, Croatia")
+    .openPopup();
 //# sourceMappingURL=script.js.map
