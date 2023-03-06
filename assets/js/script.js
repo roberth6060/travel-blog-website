b9bb2d5ce7e8285752b01a65f0430eb521830e63
Robert C. Haye <roberthaye1994@gmail.com>
2023-03-06 23:05:18 +0100

    updated folder structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,21 @@ const latitude = 45.7988904;
 const longitude = 15.8869043;
 map = L.map("map").setView([latitude, longitude], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 L.marker([latitude, longitude])
-    .addTo(map)
-    .bindPopup(L.popup({
-    maxWidth: 150,
-    minWidth: 100,
-    autoClose: false,
-    closeOnClick: false,
-}))
-    .setPopupContent("Savska opatovina 96, 10070, Zagreb, Croatia")
-    .openPopup();
-//# sourceMappingURL=script.js.map
+  .addTo(map)
+  .bindPopup(
+    L.popup({
+      maxWidth: 150,
+      minWidth: 100,
+      autoClose: true,
+      closeOnClick: true,
+    })
+  )
+  .setPopupContent("Zagreb, Croatia")
+  .openPopup();
+
+/**'back to top' link at the
+bottom of them. Consider a navigation system with fixed positioning. */
